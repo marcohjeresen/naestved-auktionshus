17da29cc40bb9e29041ce6a41371717804f85416
markhjeresen@gmail.com <markhjeresen@gmail.com@469001a2-3586-0675-3f23-8146619e282a>
2014-01-17 09:08:48 +0000

    startbid

--- Neastved-Aktionshus/src/model/Main.java
+++ Neastved-Aktionshus/src/model/Main.java
@@ -3,23 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-
 
 /**
  *
  * @author markh_000
  */
 public class Main {
+
     public static void main(String[] args) {
         User Kurt = new User("Kurt", "Parkvej 170", 24243546);
         User Bent = new User("Bent", "Nøregade", 464646464);
-        User Jens = new User("Bent", "Nøregade", 464646464);
-        
+        User Jens = new User("Jens", "Nøregade", 464646464);
+
         ArrayList<User> userList = new ArrayList<>();
         userList.add(Bent);
         userList.add(Kurt);
@@ -27,7 +26,7 @@ public class Main {
         
         Calendar cal = Calendar.getInstance();
         cal.set(2014, 01, 20, 16, 30);
-        
+
         String manufacturer = "ItalieWine";
         String country = "Italie";
         double yearOfProduction = 1890;
@@ -38,10 +37,9 @@ public class Main {
         String title = "Wine";
         String description = "Flot Flot vin";
         double estimatedPrice = 13000;
-                
 
         Wine vin1 = new Wine(manufacturer, country, yearOfProduction, grapes, percent, bottleSize, quantity, title, description, estimatedPrice);
-        
+
         manufacturer = "Denmark Wine";
         country = "Denmark";
         yearOfProduction = 1700;
@@ -53,62 +51,65 @@ public class Main {
         description = "Gammel vin fra mormorstid";
         estimatedPrice = 15000;
         Wine vin2 = new Wine(manufacturer, country, yearOfProduction, grapes, percent, bottleSize, quantity, title, description, estimatedPrice);
-        
+
         String materials = "Bøetræ";
         String dimensions = "50 * 200";
         title = "Furniture";
         description = "Arve Stykke";
         estimatedPrice = 5000;
-        
+
         Furniture møbel = new Furniture(materials, dimensions, title, description, estimatedPrice);
-        
+
         materials = "egetræ";
         dimensions = "50 * 250";
         title = "Furniture";
         description = "Nedslidt sofa";
         estimatedPrice = 1000;
-        
+
         Furniture møbel2 = new Furniture(materials, dimensions, title, description, estimatedPrice);
-        
+
         String metal = "Sølv";
         String gemstone = "Diamant";
         String stamp = "Bente";
         title = "Jewellery";
         description = "Mega flot ring";
         estimatedPrice = 2000;
-        
-        
+
         Jewellery smykke1 = new Jewellery(metal, gemstone, stamp, title, description, estimatedPrice);
-        
+
         metal = "Guld";
         gemstone = "Ædelsten";
         stamp = "Gucci";
         title = "Jewellery";
         description = "Gucci ring";
         estimatedPrice = 10000;
-        
+
         Jewellery smykke2 = new Jewellery(metal, gemstone, stamp, title, description, estimatedPrice);
-        
-            String artist = "Vincent Van Gogh";
-            int year = 1887;
-            String style = "Den afslappede stil" ;
-            String size = "1 * 1";
-            title = "Painting";
-            description = "Super godt afslappede billede malet af selveste Vincent Van Gogh";
-            estimatedPrice = 10000000;
-        
+
+        String artist = "Vincent Van Gogh";
+        int year = 1887;
+        String style = "Den afslappede stil";
+        String size = "1 * 1";
+        title = "Painting";
+        description = "Super godt afslappede billede malet af selveste Vincent Van Gogh";
+        estimatedPrice = 10000000;
+
         Painting billede1 = new Painting(artist, year, style, size, title, description, estimatedPrice);
-        
-            artist = "Vincent Van Gogh";
-            year = 1889;
-            style = "Den naturlige stil" ;
-            size = "2 * 2";
-            title = "Painting";
-            description = "Masser af skov på dette mægtige billede af Vincent Van Gogh";
-            estimatedPrice = 5000000;
-        
+
+        artist = "Vincent Van Gogh";
+        year = 1889;
+        style = "Den naturlige stil";
+        size = "2 * 2";
+        title = "Painting";
+        description = "Masser af skov på dette mægtige billede af Vincent Van Gogh";
+        estimatedPrice = 5000000;
+
         Painting billede2 = new Painting(artist, year, style, size, title, description, estimatedPrice);
         
+        Auction auk1 = new Auction(vin1, Jens, 1000, cal.getTime());
+        cal.set(2014, 02, 15, 15, 00);
+        Auction auk2 = new Auction(vin2, Jens, 1500, cal.getTime());
+        cal.set(2014, 02, 15, 17, 00);
         Auction auk3 = new Auction(smykke1, Jens, 1000, cal.getTime());
         cal.set(2014, 01, 19, 12, 10);
         Auction auk4 = new Auction(smykke2, Jens, 5000, cal.getTime());
@@ -116,6 +117,25 @@ public class Main {
         Auction auk5 = new Auction(billede1, Jens, 8000000, cal.getTime());
         cal.set(2014, 01, 20, 15, 15);
         Auction auk6 = new Auction(billede2, Jens, 4000000, cal.getTime());
+        cal.set(2014, 02, 13, 11, 00);
+        Auction auk7 = new Auction(møbel, Jens, 900, cal.getTime());
+        cal.set(2014, 03, 13, 16, 00);
+        Auction auk8 = new Auction(møbel2, Jens, 500, cal.getTime());
+        
+        ArrayList<Auction> auctionList =  new ArrayList<>();
+        auctionList.add(auk1);
+        auctionList.add(auk2);
+        auctionList.add(auk3);
+        auctionList.add(auk4);
+        auctionList.add(auk5);
+        auctionList.add(auk6);
+        auctionList.add(auk7);
+        auctionList.add(auk8);
+        
+        
+        
+        
+        
         
     };
         
--- Neastved-Aktionshus/src/view/MainFrame.java
+++ Neastved-Aktionshus/src/view/MainFrame.java
@@ -30,6 +30,7 @@ public class MainFrame extends javax.swing.JFrame {
 
         jComboBox1 = new javax.swing.JComboBox();
         jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -37,12 +38,19 @@ public class MainFrame extends javax.swing.JFrame {
 
         jButton1.setText("jButton1");
 
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        jLabel1.setText("Næstved Aktionshus");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(123, 123, 123)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 349, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(143, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(417, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jButton1)
@@ -51,11 +59,13 @@ public class MainFrame extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(25, 25, 25)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
-                .addContainerGap(322, Short.MAX_VALUE))
+                .addContainerGap(246, Short.MAX_VALUE))
         );
 
         pack();
@@ -99,5 +109,6 @@ public class MainFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JComboBox jComboBox1;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
