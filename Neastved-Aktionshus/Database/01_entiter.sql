2900a9d25f6fd4d14771fc3be3288d3d1c01619a
Bamsevand@gmail.com <Bamsevand@gmail.com@469001a2-3586-0675-3f23-8146619e282a>
2014-01-27 10:19:47 +0000

    ekstra sql script!

--- Neastved-Aktionshus/Database/01_entiter.sql
+++ Neastved-Aktionshus/Database/01_entiter.sql
@@ -3,43 +3,64 @@ create database na;
 use na;
 
 drop table if exists product;
-create table product(
-p_name varchar (255) not null,
-p_description varchar(255) not null,
-p_estimatedPrice int not null,
-p_picturePath varchar(255) not null,
-primary key (p_name, p_description, p_estimatedPrice, p_picturePath)
-) engine = InnoDB;
+create table product (
+    p_name varchar(255) not null,
+    p_description varchar(255) not null,
+    p_estimatedPrice int not null,
+    p_picturePath varchar(255) not null,
+    primary key (p_name , p_description , p_estimatedPrice , p_picturePath)
+)  engine=InnoDB;
 
 drop table if exists wine;
-create table wine(
-manufacturer varchar (255) not null,
-country varchar(255) not null,
-productionYear int not null,
-grapes varchar(255) not null,
-percent double not null,
-botttleSize double not null,
-quantity int not null
-) engine = InnoDB;
+create table wine (
+    manufacturer varchar(255) not null,
+    country varchar(255) not null,
+    productionYear int not null,
+    grapes varchar(255) not null,
+    percent double not null,
+    botttleSize double not null,
+    quantity int not null
+)  engine=InnoDB;
 
 drop table if exists furniture;
-create table furniture(
-materials varchar (255) not null,
-dimensions varchar(40) not null
-) engine = InnoDB;
+create table furniture (
+    materials varchar(255) not null,
+    dimensions varchar(40) not null
+)  engine=InnoDB;
 
 drop table if exists painting;
-create table painting(
-artist varchar (255) not null,
-productionYear int not null,
-style varchar(255) not null,
-size varchar(255) not null
-) engine = InnoDB;
+create table painting (
+    artist varchar(255) not null,
+    productionYear int not null,
+    style varchar(255) not null,
+    size varchar(255) not null
+)  engine=InnoDB;
 
 drop table if exists jewellery;
-create table jewellery(
-metal varchar (255) not null,
-gemstone varchar(255) not null,
-stamp varchar(255) not null
-) engine = InnoDB;
+create table jewellery (
+    metal varchar(255) not null,
+    gemstone varchar(255) not null,
+    stamp varchar(255) not null
+)  engine=InnoDB;
+
+drop table if exists user;
+create table user (
+    userName varchar(255) not null,
+    address varchar(255) not null,
+    phoneNumber int not null,
+    primary key (userName , address , phoneNumber)
+)  engine=InnoDB;
+
+drop table if exists bid;
+create table bid (
+    amount double not null,
+    bid_time datetime not null
+)  engine=InnoDB;
+
+drop table if exists auction;
+create table auction (
+    startBid double not null,
+    endTime datetime not null
+)  engine=InnoDB;
+
 
